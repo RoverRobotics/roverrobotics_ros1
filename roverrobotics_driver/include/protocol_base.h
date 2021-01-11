cca62720406dcbeee5abe563ac23cd6e314e90e1
John Bartholomew <john@roverrobotics.com>
2021-01-11 12:33:54 -0600

    change makefiles, still not seeing the make process use the files

--- roverrobotics_driver/include/protocol_base.h
+++ roverrobotics_driver/include/protocol_base.h
@@ -2,17 +2,17 @@
 
 class BaseProtocolObject {
     public:
-    virtual void update_drivetrim();
-    virtual void translate_send_estop();
-    virtual void translate_send_state_request();
-    virtual void translate_send_robot_info_request();
-    virtual void handle_unsupported_ros_message();
-    virtual void unpack_robot_response();
-    virtual void register_state_response_cb(boost::function<int(void)> _f);
-    virtual void register_comm_manager();
+        virtual void update_drivetrim();
+        virtual void translate_send_estop();
+        virtual void translate_send_state_request();
+        virtual void translate_send_robot_info_request();
+        virtual void handle_unsupported_ros_message();
+        virtual void unpack_robot_response();
+        virtual void register_state_response_cb(boost::function<int(void)> _f);
+        virtual void register_comm_manager();
     private:
-    comm_manager_t comm_manager;
-    mutex comm_manager_mutex;
-    void (*state_response_cb_function)(); 
+        comm_manager_t comm_manager;
+        mutex comm_manager_mutex;
+        void (*state_response_cb_function)(); 
     
 }
