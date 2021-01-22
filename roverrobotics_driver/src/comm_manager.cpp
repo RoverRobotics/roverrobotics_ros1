79a3e1829e8f778e137104a50d4e660e8d65228e
William Rook <rook0020@umn.edu>
2021-01-22 13:51:58 -0600

    syntax

--- roverrobotics_driver/src/comm_manager.cpp
+++ roverrobotics_driver/src/comm_manager.cpp
@@ -1,16 +1,16 @@
 #include "comm_manager.hpp"
 
-void *readThread(void *arg) {
+void RoverRobotics::CommManager::*readThread(void *arg) {
     clock_t t;
     std::cout << t.clock() << std::endl;
 }
 
-void *writeThread(std::string msg) {
+void RoverRobotics::CommManager::*writeThread(std::string msg) {
     clock_t t;
     std::cout << t.clock() << std::endl;
 }
 }
-CommManager(const std::string &device, float baudRate) {
+RoverRobotics::CommManager::CommManager(const std::string &device, float baudRate) {
     pthread_t write_Thread;
     int ret_write;
     pthread_t read_Thread;
@@ -18,7 +18,8 @@ CommManager(const std::string &device, float baudRate) {
     ret_write = pthread_create(&write_Thread, NULL, &writeThread(), NULL);
     ret_read = pthread_create(&read_Thread, NULL, &readThread(), NULL);
 }
-void connect(std::string port) {
+RoverRobotics::CommManager::~CommManager() {}
+void RoverRobotics::CommManager::connect(std::string port) {
     serial_port = open(port, O_RDWR);
     if (serial_port < 0) {
         printf("Error %i from open: %s\n", errno, strerror(errno));
