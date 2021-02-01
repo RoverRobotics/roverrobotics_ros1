1800075311511f0ae00fbec5af75eeff6defbcc8
drhieu <will@roverrobotics.com>
2021-02-01 13:47:40 -0600

    more debug

--- roverrobotics_driver/src/ros_top_node.cpp
+++ roverrobotics_driver/src/ros_top_node.cpp
@@ -194,13 +194,15 @@ void RoverRobotics::ROSWrapper::publishRobotInfo() {
 // call everytime speed_topic_ get data
 void RoverRobotics::ROSWrapper::callbackSpeedCommand(
     const geometry_msgs::Twist &msg) {
+      ROS_INFO("trying to update speed");
   if (!estop_state) {
     double speeddata[2];
     speeddata[0] = msg.linear.x;
     speeddata[1] = msg.angular.z;
     speeddata[2] = msg.angular.y;
-    robot_->translate_send_speed(speeddata);
     ROS_INFO("sent %f %f %f to the robot", msg.linear.x, msg.angular.z, msg.angular.y);
+    robot_->translate_send_speed(speeddata);
+    
   }else
   {
     robot_->translate_send_estop();
