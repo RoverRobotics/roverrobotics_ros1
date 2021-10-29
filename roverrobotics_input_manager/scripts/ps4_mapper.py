1aa19f2f987ce363f9801dabbdfc2e9488b27101
drhieu <will@roverrobotics.com>
2021-10-29 12:21:09 -0500

    fixed mfg added turning scale

--- roverrobotics_input_manager/scripts/ps4_mapper.py
+++ roverrobotics_input_manager/scripts/ps4_mapper.py
@@ -115,6 +115,19 @@ class ps4_mapper(object):
                 self._pub_feedback.publish(self._feedback)
             rospy.loginfo('Linear Scale is at %f', self._scales["linear"]["x"])
             self.buttonpressed = True
+        if (msg.button_dpad_left or msg.button_dpad_right) and self.buttonpressed is False:
+            if msg.button_dpad_right and self._scales["angular"].get("z") < 3.14:
+                self._scales["angular"]["z"] += 0.05
+            elif msg.button_dpad_left and self._scales["angular"].get("z") > 0.05:
+                self._scales["angular"]["z"] -= 0.05
+            elif self._scales["angular"].get("z") <= 0.06 or self._scales["angular"].get("z") >= 3.14:
+                self._feedback.set_rumble = True
+                rospy.loginfo("Limit Reach %f",
+                              self._scales["angular"].get("z"))
+                self._feedback.rumble_big = 1
+                self._pub_feedback.publish(self._feedback)
+            rospy.loginfo('Angular Scale is at %f', self._scales["angular"]["z"])
+            self.buttonpressed = True
         button_msg = Bool()
         button_msg.data = False
         if msg.button_cross:
